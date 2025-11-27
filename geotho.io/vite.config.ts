426063c756b08f838c566e0952f22794b5026218
George Thomas <georgetamayothomas@gmail.com>
2025-11-27 09:53:40 +0000

    Configure MDX math pipeline with KaTeX

--- geotho.io/vite.config.ts
+++ geotho.io/vite.config.ts
@@ -4,15 +4,17 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
-    tailwindcss(), 
-    mdx({ 
-      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-      rehypePlugins: [] 
-    })
+    react(),
+    tailwindcss(),
+    mdx({
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter, remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
   ],
 });
